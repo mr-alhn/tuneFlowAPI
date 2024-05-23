ba408017fb4a7375653adf62947a5cf49f6a6d4b
Deepak Kushwaha <djdeepak4253@gmail.com>
2024-05-23 14:18:07 +0530

    #

--- models/playlist.js
+++ models/playlist.js
@@ -13,24 +13,18 @@ const Playlist = db.define("playlist", {
   },
   songs: {
     type: DataTypes.JSON,
-    allowNull: false,
-    defaultValue: [],
     get() {
       return JSON.parse(this.getDataValue("songs"));
     },
   },
   songIds: {
     type: DataTypes.JSON,
-    allowNull: false,
-    defaultValue: [],
     get() {
       return JSON.parse(this.getDataValue("songIds"));
     },
   },
   users: {
     type: DataTypes.JSON,
-    allowNull: false,
-    defaultValue: [],
     get() {
       return JSON.parse(this.getDataValue("songIds"));
     },
